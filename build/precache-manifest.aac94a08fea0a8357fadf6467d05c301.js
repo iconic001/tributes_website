b2394c01966803dbc79a889edf2521c5cafe7667
Moses <mosesrng@gmail.com>
2022-09-07 19:04:18 +0300

    first commit

---
+++ build/precache-manifest.aac94a08fea0a8357fadf6467d05c301.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a11127e62488166c4330f5e648194b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f41a77e83a4ead61889c",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41a77e83a4ead61889c",
+    "url": "/static/js/main.45ceb677.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "6f106dbf6ee464923edf20cbe1303dc3",
+    "url": "/static/media/ann_Ruhiu.6f106dbf.png"
+  },
+  {
+    "revision": "84e205b0536b2172de2f6fc07d8538bc",
+    "url": "/static/media/dr_Ann.84e205b0.png"
+  }
+]);
